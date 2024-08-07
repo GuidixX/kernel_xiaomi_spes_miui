4e121b03bf104f3dc8cb9007766ccb378483f65b
shifty-d <ffoxy2870@gmail.com>
2024-08-07 10:37:02 +0300

    Reapply "test"
    This reverts commit 2d734e8b85c028556001797f6440f7f64dddadc3.

--- drivers/power/supply/nopmi_chg_common.c
+++ drivers/power/supply/nopmi_chg_common.c
@@ -17,7 +17,7 @@ int nopmi_chg_is_usb_present(struct power_supply *usb_psy)
 	{
 		usb_psy = power_supply_get_by_name("usb");
 		if (!usb_psy) {
-			pr_err("usb supply not found, defer probe\n");
+			("usb supply not found, defer probe\n");
 			return -EINVAL;
 		}
 	}
@@ -25,7 +25,7 @@ int nopmi_chg_is_usb_present(struct power_supply *usb_psy)
 	ret = power_supply_get_property(usb_psy,
 				POWER_SUPPLY_PROP_ONLINE, &prop);
 	if (ret < 0) {
-		pr_err("couldn't read usb_present property, ret=%d\n", ret);
+		("couldn't read usb_present property, ret=%d\n", ret);
 		return -EINVAL;
 	}
 	usb_present = prop.intval;
@@ -46,7 +46,7 @@ char nopmi_set_charger_ic_type(NOPMI_CHARGER_IC_TYPE nopmi_type)
 	{
 		ret = -ENODEV;
 	}
-	pr_err("2012.09.04 wsy %s: start nopmi_type=%d nopmi_charger_ic=%d\n", __func__, nopmi_type, nopmi_charger_ic);
+	("2012.09.04 wsy %s: start nopmi_type=%d nopmi_charger_ic=%d\n", __func__, nopmi_type, nopmi_charger_ic);
 	return ret;
 }
 
@@ -105,7 +105,7 @@ int nopmi_get_quick_charge_type(struct power_supply *usb_psy)
 	{
 		usb_psy = power_supply_get_by_name("usb");
 		if (!usb_psy) {
-			pr_err("usb supply not found, defer probe\n");
+			("usb supply not found, defer probe\n");
 			return -EINVAL;
 		}
 	}
@@ -113,7 +113,7 @@ int nopmi_get_quick_charge_type(struct power_supply *usb_psy)
 	ret = power_supply_get_property(usb_psy,
 				POWER_SUPPLY_PROP_REAL_TYPE, &prop);
 	if (ret < 0) {
-		pr_err("couldn't read usb real type, ret=%d\n", ret);
+		("couldn't read usb real type, ret=%d\n", ret);
 		return -EINVAL;
 	}
 	chg_type = prop.intval;
@@ -122,28 +122,28 @@ int nopmi_get_quick_charge_type(struct power_supply *usb_psy)
 	{
 		batt_psy = power_supply_get_by_name("battery");
 		if(!batt_psy){
-			pr_err("battery supply not found, defer probe\n");
+			("battery supply not found, defer probe\n");
 			return -EINVAL;
 		}
 	}
 
 	ret = power_supply_get_property(batt_psy, POWER_SUPPLY_PROP_TEMP, &prop);
 	if (ret < 0) {
-		pr_err("couldn't read batt temp property, ret=%d\n", ret);
+		("couldn't read batt temp property, ret=%d\n", ret);
 		return -EINVAL;
 	}
 
-	pr_info("battery temp: %d", prop.intval);
+	("battery temp: %d", prop.intval);
 	if (prop.intval < 50 || prop.intval >= 480) {
 		if (usb_psy && !is_single_flash){
-			pr_info("battery temp is under 5 or above 48");
+			("battery temp is under 5 or above 48");
 			power_supply_changed(usb_psy);
 		}
 		is_single_flash = true;
 		return 0;
 	} else {
 		if (usb_psy && is_single_flash){
-			pr_info("battery temp returned to normal");
+			("battery temp returned to normal");
 			power_supply_changed(usb_psy);
 		}
 		is_single_flash = false;
--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 /* This file is auto generated, version 17 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
 #define UTS_VERSION "#17 SMP PREEMPT Wed Aug 7 10:23:57 EEST 2024"
+=======
+/* This file is auto generated, version 12 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#12 SMP PREEMPT Tue Aug 6 22:01:58 EEST 2024"
+>>>>>>> parent of 2d734e8b85c0 (Revert "test")
 #define LINUX_COMPILE_BY "hallt"
 #define LINUX_COMPILE_HOST "localhost"
 #define LINUX_COMPILER "Android (8508608, based on r450784e) clang version 14.0.7 (https://android.googlesource.com/toolchain/llvm-project 4c603efb0cca074e9238af8b4106c30add4418f6), LLD 14.0.7"
